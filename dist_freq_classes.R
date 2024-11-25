71484a341bee07f12499db75fa80e537445526e3
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-11-25 12:36:01 -0300

    Update dist_freq_classes.R

--- dist_freq_classes.R
+++ dist_freq_classes.R
@@ -1,27 +1,27 @@
 rm(list = ls())
-
 dados = read.table("tabela1.csv", sep=";", header=TRUE, stringsAsFactors=TRUE)
 
-x = dados[["Idade"]]
+x = dados[["Idade"]] # variavel analisada
 xmax = max(x)
 xmin = min(x)
+AT = xmax-xmin # amplitude total dos dados
 
-AT = xmax-xmin
-n = length(x)
-k = round(sqrt(n))
-h = round(AT/k)
+n = length(x) # tamanho da amostra
+k = round(sqrt(n)) # numero de classes
+h = round(AT/k) # amplitude das classes
 
 xmax = xmax +1
-x_lims = seq(xmin,xmax,h) # limites dos intervalos
 
+x_lims = seq(xmin,xmax,h) # limites dos intervalos 
 x_intervals = cut(x, breaks=x_lims, right=FALSE) # intervalos em si , com o elemento da direita fora do intervalo.
+# li->|Li
 
 Fi = table(x_intervals) 
 fi = 100 * prop.table(Fi)
 Fai = cumsum(Fi)
 fai = cumsum(fi)
 
-xi = seq(xmin+h/2,xmax,h) # pontos medios do histograma!
+xi = seq(xmin+h/2,xmax,h) # pontos medios das classes
 
 tab = cbind(xi,Fi,fi,Fai,fai)
 round(tab, digits=2)
